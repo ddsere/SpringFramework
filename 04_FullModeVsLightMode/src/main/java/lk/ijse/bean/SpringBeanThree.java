3372d2ac95090891b3b8eb74e833526832dab66a
Daham <157113991+ddsere@users.noreply.github.com>
2026-01-14 15:31:17 +0530

    FullMode Vs LiteMode

--- 04_FullModeVsLightMode/src/main/java/lk/ijse/bean/SpringBeanThree.java
+++ 04_FullModeVsLightMode/src/main/java/lk/ijse/bean/SpringBeanThree.java
@@ -1,7 +1,25 @@
 package lk.ijse.bean;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBeanThree {
+    /*//FullMode
+    @Bean
+    public SpringBeanOne springBeanOne() {
+        //Inter-bean dependency = not satisfied
+        //invoking bean method inside another bean method
+        SpringBeanTwo springBeanTwo1 = new SpringBeanTwo();
+        SpringBeanTwo springBeanTwo2 = new SpringBeanTwo();
+        System.out.println(springBeanTwo1);
+        System.out.println(springBeanTwo2);
+        return new SpringBeanOne();
+
+    }
+
+    @Bean
+    public SpringBeanTwo springBeanTwo() {
+        return new SpringBeanTwo();
+    }*/
 }
--- 04_FullModeVsLightMode/src/main/java/lk/ijse/config/AppConfig.java
+++ 04_FullModeVsLightMode/src/main/java/lk/ijse/config/AppConfig.java
@@ -1,9 +1,22 @@
 package lk.ijse.config;
 
+import lk.ijse.bean.SpringBeanOne;
+import lk.ijse.bean.SpringBeanTwo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.bean")
 public class AppConfig {
+
+    @Bean
+    public SpringBeanOne springBeanOne() {
+        return new SpringBeanOne();
+    }
+
+    @Bean
+    public SpringBeanTwo springBeanTwo() {
+        return new SpringBeanTwo();
+    }
 }
