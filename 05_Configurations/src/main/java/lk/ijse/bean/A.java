39f736af47759433f92de07f50b70c93a6d89d7f
Daham <157113991+ddsere@users.noreply.github.com>
2026-01-21 10:06:39 +0530

    Configurations

---
+++ 05_Configurations/src/main/java/lk/ijse/bean/A.java
@@ -0,0 +1,10 @@
+package lk.ijse.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class A {
+    public A (){
+        System.out.println("A Object Created");
+    }
+}
