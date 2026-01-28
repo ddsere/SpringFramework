1faa327d8026b098583e07eca68d9b7e069592c5
Daham <157113991+ddsere@users.noreply.github.com>
2026-01-28 10:29:53 +0530

    Spring web mvc

--- 07_Spring_Env/src/main/java/lk/ijse/bean/SpringBean.java
+++ 07_Spring_Env/src/main/java/lk/ijse/bean/SpringBean.java
@@ -1,7 +1,25 @@
 package lk.ijse.bean;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
+public class SpringBean implements InitializingBean {
+    @Value("${db.name}")
+    private String dbName;
+    @Value("${db.username}")
+    private String dbUser;
+    @Value("${db.password}")
+    private String dbPassword;
+    @Value("${db.url}")
+    private String dbURL;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(dbName);
+        System.out.println(dbUser);
+        System.out.println(dbPassword);
+        System.out.println(dbURL);
+    }
 }
--- 08_SpringWebMVC/src/main/java/lk/ijse/config/WebAppConfig.java
+++ 08_SpringWebMVC/src/main/java/lk/ijse/config/WebAppConfig.java
@@ -2,11 +2,10 @@ package lk.ijse.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.bean")
-@PropertySource("classpath:application.properties")
-public class AppConfig {
+@EnableWebMvc
+public class WebAppConfig {
 }
